490ec02c9a6cbef7a6abd0d29d79493f74333381
cener78 <94464442+cener78@users.noreply.github.com>
2022-08-30 00:15:32 +0200

    delete method has been done

--- MolenTaskSolution/Pages/Task_Panel.cs
+++ MolenTaskSolution/Pages/Task_Panel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using MolenTaskSolution.Dialogs;
 using MolenTaskSolution.Models;
 using System;
@@ -11,6 +12,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MolenTaskSolution.Pages
 {
@@ -45,7 +48,7 @@ namespace MolenTaskSolution.Pages
         }
         void Update_UI()
         {
-            //SqlConnection con = new SqlConnection("Server=144.126.158.15,1433;Database=molendb; User ID=administrator; Password=Qwert1234");
+            //SqlConnection con = new SqlConnection("Server=144.126.158.15,1433;Database=dbmolen; User ID=administrator; Password=Qwert1234");
             //string sqlQuery = "SELECT Projects.ProjectName, Tasks.TaskName, Users.UserName, Tasks.Status,Tasks.StartDate, Tasks.CompletionDate FROM [Tasks] JOIN [Projects] ON Tasks.ProjectID = Projects.ProjectID JOIN [Users] ON Tasks.TaskOwnerID = Users.UserID";
             //SqlCommand com = new SqlCommand(sqlQuery, con);
             //con.Open();
@@ -101,7 +104,7 @@ namespace MolenTaskSolution.Pages
 
         }
 
-        private void btnDelete_Click(object sender, EventArgs e)
+        private  void btnDelete_Click(object sender, EventArgs e)
         {
             if (dgwTaskPanel.SelectedRows.Count == 0)
             {
@@ -110,12 +113,66 @@ namespace MolenTaskSolution.Pages
             }
             if (MessageBox.Show("Are you sure you want to delete this record ?", "EF CRUD Operation", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
-                
+                string selectedName="";
+                foreach (DataGridViewRow row in dgwTaskPanel.SelectedRows)  
+                {
 
-                var removedTask = dgwTaskPanel.SelectedRows[0].DataBoundItem;
+
+                    selectedName = Convert.ToString(row.Cells[1].Value);
+                    
+                }
+              
+               // MessageBox.Show(selectedName);
+
+                SqlConnection con = new SqlConnection("Server=144.126.158.15,1433;Database=dbmolen; User ID=administrator; Password=Qwert1234");
+                con.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM Tasks  WHERE TaskName = '"+selectedName+"';", con); 
+                command.ExecuteNonQuery();
+                con.Close();
+
+
+
+               
+
+
+               /* var deleteTasks =
+                     from t in model.Tasks
+                     where t.TaskName == selectedName
+                         select t;
+
+                var silinecek_task = model.Tasks.Where(t => t.TaskName == selectedName).FirstOrDefault(); 
+                // islem sirasina gore selectedName forloopun disina cikip yeni deger alincaya kadar null atiyor. 
+                try
+                {
+                    model.Tasks.Remove(silinecek_task);
+                    model.SaveChanges();
+
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("olmadimi yoksa");
+                }*/
+
+
+
+
+                //////////
+
+
+                /* if (this.dgwTaskPanel.SelectedRows.Count > 0)
+                 {
+                     dgwTaskPanel.Rows.RemoveAt(this.dgwTaskPanel.SelectedRows[0].Index);
+                 }*/
+
+
+                //var removedTask = dgwTaskPanel.SelectedRows[0].DataBoundItem;
 
             }
 
         }
+
+      
+
+
+    
     }
 }
